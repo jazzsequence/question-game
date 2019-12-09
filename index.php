f780bee2c102c81e11209825b833c0f91e31a89b
Chris Reynolds <chris@humanmade.com>
2019-12-09 15:49:16 -0700

    add initial structure

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Question Game
+ * 
+ * Prototype PHP-based question game framework.
+ * 
+ * @package Question-Game
+ * @author Chris Reynolds <chris@jazzsequence.com>
+ * @version 0.1.0
+ */
+
+namespace Game;
+
+require_once __DIR__ . '/src/bootstrap.php';
---
+++ src/inc/functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Question Game Functions
+ * 
+ * Helper functions used in the game files.
+ *
+ * @package Question-Game
+ * @author Chris Reynolds <chris@jazzsequence.com>
+ * @version 0.1.0
+ */
