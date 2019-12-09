a0377a6d92b886b23c1cfc89e7634a3ee8d72662
Chris Reynolds <chris@humanmade.com>
2019-12-09 16:38:52 -0700

    fix linting

--- src/inc/functions.php
+++ src/inc/functions.php
@@ -1,10 +1,12 @@
 <?php
 /**
  * Question Game Functions
- * 
+ *
  * Helper functions used in the game files.
  *
  * @package Question-Game
  * @author Chris Reynolds <chris@jazzsequence.com>
  * @version 0.1.0
  */
+
+namespace Game;
