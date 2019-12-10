3f2e661b80c477143ff6f0ac45892427b94495a3
Chris Reynolds <chris@humanmade.com>
2019-12-10 13:37:00 -0700

    the main wrapper template

---
+++ src/templates/main.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Main wrapper template
+ * 
+ * Renders the contents of the page and calls in additional templates.
+ * 
+ * @package Question-Game
+ * @author Chris Reynolds <chris@jazzsequence.com>
+ * @version 0.0
+ */
+
+namespace Game;
+
+get_head();
+?>
+
+<div class="question-game main">
+	<div class="alert alert-notify">
+		<p>This game uses cookies to store information. Please make sure your browser is able to save cookies and click the New Game button to start a new session.</p>
+	</div>
+
+	<div class="new-game">
+		<button type="button" class="nes-btn is-primary new-game">New Game</button>
+	</div>
+
+	<!-- Stuff goes here. -->
+
+</div>
+
+<?php
+get_footer();
