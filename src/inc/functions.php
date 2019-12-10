b76b84def9a1a29ec3f78876a533a55883fcb258
Chris Reynolds <chris@humanmade.com>
2019-12-10 13:36:30 -0700

    add a function to output the head markup

--- src/inc/functions.php
+++ src/inc/functions.php
@@ -70,3 +70,24 @@ function assets_url( string $file = '', string $assets = '' ) : string {
 	return $domain . $assets . $file;
 }
 
+/**
+ * Output the head section of the page.
+ *
+ * @param string $title (Optional) A page title to display in the browser window/tab.
+ */
+function get_head( string $title = '' ) {
+	$title = ! empty( $title ) ? $title : 'Question Game';
+	?>
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title><?php echo $title; ?></title>
+		<link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="<?php echo assets_url( 'nes.min.css', NES_CSS ); ?>">
+		<link rel="stylesheet" type="text/css" href="<?php echo assets_url( 'css/style.css' ); ?>">
+	</head>
+	<body>
+	<?php
+}
+
