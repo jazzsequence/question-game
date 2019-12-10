8997a00f48789f24186d4b98f1c066ccd7d89878
Chris Reynolds <chris@humanmade.com>
2019-12-10 13:59:25 -0700

    add a container around the alert
    todo: maybe make a separate alert class?
    todo: position this at the bottom? in a dismissable window?

--- src/templates/main.php
+++ src/templates/main.php
@@ -15,7 +15,7 @@ get_head();
 ?>
 
 <div class="question-game main">
-	<div class="alert alert-notify">
+	<div class="nes-container nes-text is-primary">
 		<p>This game uses cookies to store information. Please make sure your browser is able to save cookies and click the New Game button to start a new session.</p>
 	</div>
 
