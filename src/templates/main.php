f451f5aa55aff764f15d6433f97dddfe1440b63c
Chris Reynolds <chris@humanmade.com>
2019-12-10 14:11:31 -0700

    add a unique class to the cookie alert

--- src/templates/main.php
+++ src/templates/main.php
@@ -15,7 +15,7 @@ get_head();
 ?>
 
 <div class="question-game main">
-	<div class="nes-container nes-text is-primary">
+	<div class="nes-container nes-text is-primary cookie-alert">
 		<p>This game uses cookies to store information. Please make sure your browser is able to save cookies and click the New Game button to start a new session.</p>
 	</div>
 
