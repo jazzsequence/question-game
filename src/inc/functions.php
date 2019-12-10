89973e4f606d9691d148249d833dcbc7edc7244a
Chris Reynolds <chris@humanmade.com>
2019-12-10 10:35:08 -0700

    adds a helper function that returns the data from the json files

--- src/inc/functions.php
+++ src/inc/functions.php
@@ -10,3 +10,19 @@
  */
 
 namespace Game;
+
+/**
+ * Gets all the data from json files in the /data/ directory.
+ * 
+ * @return array An array of all the data from the various json files.
+ */
+function get_data() : array {
+	$data = [];
+
+	foreach ( glob( DATA_DIR . '*.json' ) as $datasrc ) {
+		$dataset = json_decode( $datasrc, true );
+		$data    = array_merge( $data, $dataset );
+	}
+
+	return $data;
+}
