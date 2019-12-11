da544f00acf857579d6e0125e69e318b88c2e891
Chris Reynolds <chris@humanmade.com>
2019-12-11 13:10:30 -0700

    remove cookie code that we're not using

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,3 @@
-/* globals require */
 /**
  * Question Game javascript
  *
@@ -7,27 +6,10 @@
 const qstGameInit = () => {
 	const alert = document.querySelector( '.cookie-alert' );
 	const alertBtn = document.querySelector( 'button.cookie-confirm' );
-	const cookie = qstGameGetCookie();
-
-	console.log( eval( `(${cookie})` ) );
 
 	alertBtn.addEventListener( 'click', () => {
 		alert.style.display = 'none';
 	} );
-}
-
-const qstGameGetCookie = () => {
-	const value = '; ' + document.cookie;
-	const parts = value.split( '; qst_game=' );
-	if ( parts.length == 2 ) {
-		return parts.pop()
-			.split( ';' )
-			.shift();
-	}
-}
-
-const qstGameUpdateCookie = ( cookie ) => {
-
 }
 
 qstGameInit();
