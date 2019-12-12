cc675b62ece6e54fe749d3156ef6e99aa06f9a7e
Chris Reynolds <chris@humanmade.com>
2019-12-11 17:11:24 -0700

    add a level section

---
+++ src/templates/molecules/level.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Current Question and Level
+ *
+ * @package Question-Game
+ * @author Chris Reynolds <chris@jazzsequence.com>
+ * @version 0.1.0
+ */
+
+namespace Game;
+
+?>
+<div class="level-question">
+	<p>
+		<span class="nes-text is-success"><?php echo get_level_name( get_current_level() ); ?></span>
+	</p>
+</div>
