16c8a673c121a70ab96c78721e256831a039ecca
Chris Reynolds <chris@jazzsequence.com>
2024-02-12 11:14:44 -0700

    refactored gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,45 @@
-const gulp = require( 'gulp' ),
-	sass = require( 'gulp-sass' ),
-	del = require( 'del' );
+const gulp = require('gulp'),
+      sass = require('gulp-sass')(require('sass')),
+      fs = require('fs'),
+      path = require('path');
 
-gulp.task( 'styles', () => {
-	return gulp.src( 'src/assets/sass/**/*.scss' )
-		.pipe( sass().on( 'error', sass.logError ) )
-		.pipe( gulp.dest( './src/assets/css/' ) );
-} );
+// Simple clean function using fs
+function clean(done) {
+  const filePath = path.join(__dirname, 'src/assets/css/styles.css');
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      // If the file does not exist, handle the error gracefully
+      if (err.code === 'ENOENT') {
+        console.log('File does not exist, skipping deletion.');
+        done();
+      } else {
+        done(err);
+      }
+    } else {
+      console.log('Successfully deleted file.');
+      done();
+    }
+  });
+}
 
-gulp.task( 'clean', () => {
-	return del( [
-		'src/assets/css/style.css',
-	] );
-} );
+gulp.task('nes.css', () => {
+	return gulp.src('vendor/jazzsequence/nes.css/scss/nes.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('src/assets/css/'));
+});
 
-gulp.task( 'build', gulp.series( [ 'clean', 'styles' ] ) );
+gulp.task('styles', () => {
+  return gulp.src('src/assets/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./src/assets/css/'));
+});
 
-gulp.task( 'watch', () => {
-	gulp.watch( 'src/assets/sass/**/*.scss', ( done ) => {
-		gulp.series( [ 'clean', 'styles' ] )( done );
-	} );
-} );
+gulp.task('clean', clean);
+
+gulp.task('build', gulp.series(['clean', 'nes.css', 'styles']));
+
+gulp.task('watch', () => {
+  gulp.watch('src/assets/sass/**/*.scss', (done) => {
+    gulp.series(['clean', 'styles'])(done);
+  });
+});
