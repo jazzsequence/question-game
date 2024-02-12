2303c7887d141f34fbaeb942819d7662c7000e26
Chris Reynolds <chris@jazzsequence.com>
2024-02-12 12:24:56 -0700

    quick linting fixes

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -16,7 +16,7 @@ define( 'SRC_DIR', ROOT_DIR . '/src/' );
 define( 'INC_DIR', SRC_DIR . 'inc/' );
 define( 'DATA_DIR', SRC_DIR . 'data/' );
 define( 'TEMPLATES_DIR', SRC_DIR . 'templates/' );
-define( 'ASSETS_PATH', dirname( __FILE__ ) . '/assets/' );
+define( 'ASSETS_PATH', __DIR__ . '/assets/' );
 define( 'NES_CSS', '/assets/css/' );
 
 /**
